c39ba1258ea07623e5a76df28355a5867c113b36
Saba-Baroughi <saba.baroughi1111@gmail.com>
2024-01-03 12:09:46 +0000

    test4

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6a1_#d31sks&n0sh)x(*_vxf3lim8xg-oabokek#u%0x+9-5d5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1' ,'.herokuapp.com']
 
